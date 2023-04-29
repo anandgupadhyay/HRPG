1d2bb1319cdcf28c7631b37c6932c4d7f66fc5d8
anandgupadhyay <anandgupadhyay03@gmail.com>
2023-04-29 09:19:20 +0530

    trying different implementations

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -54,7 +54,7 @@ func commonChild(s1: String, s2: String) -> Int {
     return maxLength
 }
 
-let result = commonChild(s1: "ABCD", s2: "ABDC")
+var result = commonChild(s1: "ABCD", s2: "ABDC")
 print("result:\(result)")
 
 func commonChild1(s1: String, s2: String) -> Int {
@@ -97,4 +97,72 @@ func commonChild1(s1: String, s2: String) -> Int {
 
 
 result = commonChild(s1: "ABCD", s2: "ABDC")
+print("result:\(result)")
+
+
+func commonChild2(s1: String, s2: String) -> Int {
+    let s1Array = Array(s1)
+    let s2Array = Array(s2)
+    
+    var memo = Array(repeating: Array(repeating: 0, count: s2.count + 1), count: s1.count + 1)
+    
+    for i in 1...s1.count {
+        for j in 1...s2.count {
+            if s1Array[i-1] == s2Array[j-1] {
+                memo[i][j] = memo[i-1][j-1] + 1
+            } else {
+                memo[i][j] = max(memo[i-1][j], memo[i][j-1])
+            }
+        }
+    }
+    
+    return memo[s1.count][s2.count]
+}
+
+result = commonChild2(s1: "ABCD", s2: "ABDC")
+print("result:\(result)")
+
+func commonChild3(s1: String, s2: String) -> Int {
+    var suffixes = Array(repeating: Array(repeating: 0, count: s2.count + 1), count: 2)
+    var activeRow = 0
+    
+    for i in s1.indices {
+        let prevRow = activeRow
+        activeRow = 1 - activeRow
+        
+        for j in s2.indices {
+            if s1[i] == s2[j] {
+                suffixes[activeRow][j+1] = suffixes[prevRow][j] + 1
+            } else {
+                suffixes[activeRow][j+1] = max(suffixes[activeRow][j], suffixes[prevRow][j+1])
+            }
+        }
+    }
+    
+    return suffixes[activeRow][s2.count]
+}
+
+result = commonChild3(s1: "ABCD", s2: "ABDC")
+print("result:\(result)")
+
+func commonChild4(s1: String, s2: String) -> Int {
+    let s1Array = Array(s1)
+    let s2Array = Array(s2)
+    
+    var memo = Array(repeating: Array(repeating: 0, count: s2.count + 1), count: s1.count + 1)
+    
+    for i in 1...s1.count {
+        memo[i] = (1...s2.count).reduce([0]) { (prev, j) in
+            if s1Array[i-1] == s2Array[j-1] {
+                return prev + [memo[i-1][j-1] + 1]
+            } else {
+                return prev + [max(memo[i-1][j], memo[i][j-1])]
+            }
+        }
+    }
+    
+    return memo[s1.count][s2.count]
+}
+
+result = commonChild4(s1: "ABCD", s2: "ABDC")
 print("result:\(result)")
